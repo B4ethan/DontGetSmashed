5699dc336c37ba4b2941e0cf7261da8946fb345c
Ethannn_boi <84193841+B4ethan@users.noreply.github.com>
2023-06-17 17:07:02 +0000

    update

--- Main.py
+++ Main.py
@@ -1,1 +1,23 @@
+import pygame
+import time
+import random
 
+WIDTH, HIGHT = 800, 1000
+WIN = pygame.display.set_mode((WIDTH, HIGHT))
+pygame.display.set_caption("Dont Get Smashed!")
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+    
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
